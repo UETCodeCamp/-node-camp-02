a5e367f16eb4fd0c5419eae6152177e66801f53c
Max <tutv95@gmail.com>
2019-04-13 21:58:37 +0700

    test

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -3,22 +3,28 @@ const request = require('request-promise-native')
 const hostApp = process.env.STUDENT_HOST || 'http://localhost:8888'
 console.log('Start running with host app:', hostApp)
 
-const instance  = request.defaults({
-    baseUrl: hostApp
+const _parseUrl = (host = '') => {
+    if (!host) return ''
+
+    return host.indexOf('http') === -1 ? `http://${host}` : host
+}
+
+const instance = request.defaults({
+    baseUrl: _parseUrl(hostApp)
 })
 
 const _request = async (options = {}) => {
     const defaultOpts = {
         uri: '/',
         method: 'GET',
-    }    
+    }
     const opts = Object.assign({}, defaultOpts, options)
 
     try {
         const response = await instance(opts)
 
         return response
-    } catch(error ) {
+    } catch (error) {
         throw error
     }
 }
